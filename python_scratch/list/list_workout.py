7ceaefe03e876fe651ae825147c3d6f56562b067
Abu <abuabrar520724@gmail.com>
2025-04-27 22:28:02 +0530

    list_workout

--- python_scratch/list/list_workout.py
+++ python_scratch/list/list_workout.py
@@ -16,3 +16,12 @@ print([1] * 3) # Repetition
 
 # Slicing
 
+print(my_list[1:4],my_list[:3], my_list[::-1]) # last on is reverse
+
+#updating values 
+my_list[1] = 23
+
+#Adding Element
+my_list.append(10) # adding at end
+my_list.insert(2,33) # add at postiong
+my_list.extend([2,4,55]) # add multiple
