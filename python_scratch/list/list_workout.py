935985e1b1d09d2f8375f2e4e9d41dd8d443a41f
Abu <abuabrar520724@gmail.com>
2025-04-27 22:20:21 +0530

    list_practice

--- python_scratch/list/list_workout.py
+++ python_scratch/list/list_workout.py
@@ -7,3 +7,12 @@ nested_list = [1,[2,3],4]
 # Accessing Element 
 print(my_list[0],my_list[-1])
 print(nested_list[1][0])
+
+# Basic operations
+print(len(my_list)) # length
+print(3 in my_list) # membership
+print([1,2] + [3,4]) # concatination
+print([1] * 3) # Repetition
+
+# Slicing
+
