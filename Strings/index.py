8f09ef3e6fefd3a7e3742e0e3b6173b1964ee790
Abu <abuabrar520724@gmail.com>
2025-05-04 18:22:40 +0530

    string_workout

--- Strings/index.py
+++ Strings/index.py
@@ -10,3 +10,42 @@ str1 = "Hello"
 str2 = "World"
 concatenated_string = str1 + ", " + str2
 print(concatenated_string)  # Hello, World
+
+# str. upper() & str.title()
+
+phrase = "i am python django react developer"
+cap_phrase = phrase.capitalize()
+
+title_pharse  = phrase.title()
+print(cap_phrase)
+print("title_phrase =", title_pharse)
+
+
+# str.strip(), str.lstrip(),and str.rstrip() (its remove spaces on the value)
+title = "   hello   hay    "
+print(title.strip()) # its remve all spaces (left and right) on the sentence
+
+print(title.lstrip())# its remove left side space only
+
+print(title.rstrip())# its remve right side space only
+
+
+# str.startwith(prefix) and str.endswith(suffix)
+filename = "Muhammed Abrar"
+start_with = filename.startswith('Muham')
+print(start_with)
+end_with = filename.endswith("abu")
+print(end_with)
+
+# str.replace(old,new)
+
+name = "abu abrar"
+replaced_name = name.replace("abu","muhammed")
+print(replaced_name)
+
+# str.find() and str.index()
+
+phrase1 = "Hey i become an ethical hacker on 2025 end"
+find = phrase1.find("hacker")
+find2 = phrase1.index("ethical")
+print(find,find2)
