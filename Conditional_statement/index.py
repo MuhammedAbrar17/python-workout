4efeb0a83ee7e46f43ce28849591b275babcd9f6
Abu <abuabrar520724@gmail.com>
2025-06-04 21:59:43 +0530

    conditional statements workout

--- Conditional_statement/index.py
+++ Conditional_statement/index.py
@@ -13,3 +13,15 @@ if grade == 'A' or grade == 'B' or grade == 'C':
     print("passing grade")
 else :
     print("falling grade")
+
+
+# challenge 3
+
+day = input("Enter a day of the week(eg : Monday)").capitalize
+
+if day == "Saturday" or day == "Sunday":
+    print("weekend")
+elif day == "Monday" or day == "Tuesday" or day == "Wednesday" or day == "Thursday" or day == "Friday":
+    print("weekday")
+else:
+    print("invalid day enterd")
