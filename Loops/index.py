b2c71e793aac3180b8661c1a67d36cc3438ed35c
Abu <abuabrar520724@gmail.com>
2025-06-22 23:50:50 +0530

    for loop

--- Loops/index.py
+++ Loops/index.py
@@ -1,3 +1,13 @@
 # Basics
 for i in range(10):
     print("Abu")
+
+# challenge print 1 to 10 
+
+for i in range(1,11):
+    print(i)
+
+#challenge 2 print 10 to 1 (in descending order)
+
+for j in range(10,0,-1):
+    print(j)
